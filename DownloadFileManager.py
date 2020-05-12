b00cf5f29110c1431067be9c1f0bdcf97b8a3cb4
Redwan <redwankhan1996@gmail.com>
2020-05-12 17:20:36 +0100

    Update DownloadFileManager.py

---
+++ DownloadFileManager.py
@@ -0,0 +1,47 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+# make sure watchdog is installed - $ python -m pip install watchdog
+
+import os 
+import json
+import time
+import filetype
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            file_name, file_extension = os.path.splitext(filename)
+            if file_extension in audioExt:
+                src = folder_to_track + "/" + filename
+                new_destination = folder_destination + "/Audio" + "/" + filename
+                os.rename(src, new_destination)
+            if file_extension == '.pdf':
+                src = folder_to_track + "/" + filename
+                new_destination = folder_destination + "/PDF" + "/" + filename
+                os.rename(src, new_destination)
+            if file_extension == '.zip':
+                src = folder_to_track + "/" + filename
+                new_destination = folder_destination + "/ZIP" + "/" + filename
+                os.rename(src, new_destination)
+            if file_extension == '.dmg':
+                src = folder_to_track + "/" + filename
+                new_destination = folder_destination + "/DMG" + "/" + filename
+                os.rename(src, new_destination)
+
+folder_to_track = "/Users/redwankhan/Downloads"
+folder_destination = "/Users/redwankhan/Downloads"
+
+audioExt = ['.mp3'] 
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+    
